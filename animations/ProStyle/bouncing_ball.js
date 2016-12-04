fe0a317c82c2dd38f71c3c1dd4d9ebf81c17559d
Gary Chamberlain <gary@pro.graphics>
2016-12-04 17:11:38 -0500

    Show the code

--- animations/ProStyle/bouncing_ball.js
+++ animations/ProStyle/bouncing_ball.js
@@ -1,40 +1,25 @@
 ProStyle.Stories.bouncing_ball = {
-    frame: { aspectRatio: 1 },
-    items: [
-        {
-            itemType: "layer",                  /* Container */
-            init: {bg: "#222", corners:50},
+    frame: { aspectRatio:1 },
+    item: {
+        itemType:"layer",  /* Container */
+        init: {bg:"#222", corners:50},
+        action: {
+            anim: { dur:8, ease: "linear" },
+            rot: 360
+        },
+        item: {
+            itemType:"layer",  /* Ball */
+            init: { bg:"#9b2", size:[40,40], corners:50, pos:[-30, 0] },
             action: {
-                anim: {
-                    dur: 8,
-                    ease: "linear"
-                },
-                rot: 360
-            },
-            item: {
-                itemType: "layer",              /* Ball */
-                init: { 
-                    bg: "#9b2",
-                    size: [40,40],
-                    corners: 50,
-                    pos: [-30,0]
-                },
-                action: {
-                    anim: {
-                        dur: 1,
-                        ease: "sine inout",
-                        repeat: [7, 'yoyo'],
-                        yoyo: true
-                    },
-                    pos: [30, 0]
-                }
+                anim: { dur:1, ease:"sine inout", repeat:[7,'yoyo'] },
+                pos: [30, 0]
             }
         }
-    ],
+    },
     controllers: [
-        { controllerType: "auto", start: true, stepAdvance: true, restart: true },
-        { controllerType: "track", color: "#9b2" },
-        { controllerType: "resize" },
-        { controllerType: "mouseWheel" }
+        { controllerType:"auto", start:true, stepAdvance:true, restart:true },
+        { controllerType:"track", color:"#9b2" },
+        { controllerType:"resize" },
+        { controllerType:"mouseWheel" }
     ]
 };
